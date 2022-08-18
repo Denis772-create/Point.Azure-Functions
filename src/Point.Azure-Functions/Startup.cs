1df1773a7f61f566f45157b14437b08072dffa1e
Denis772-create <denis_aleksandrov_12@mail.ru>
2022-08-19 03:13:20 +0500

    Add project files.

---
+++ src/Point.Azure-Functions/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using Point.Azure_Functions;
+
+[assembly: WebJobsStartup(typeof(Startup))]
+namespace Point.Azure_Functions
+{
+    public class Startup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            
+        }
+    }
+}
